35de3d4811cb273f42b01e90b47bb004d8ecf25b
Sin <mrsin0916@gmail.com>
2019-11-21 19:35:33 +0700

    Improve All
    js css html

--- resources/js/blog.js
+++ resources/js/blog.js
@@ -1,34 +1,29 @@
-const dummyBlog = {
-  image: [
-    'resources/images/dummyimage2.jpg',
-    'resources/images/dummyimage3.jpg',
-    'resources/images/dummyimage4.jpg',
-  ],
-  title: ['1. Super ultra hot posts', '2. Super ultra hot posts', '3. Super ultra hot posts'],
-  content:
-    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet,repudiandae excepturi ducimus animi omnis rerum? Accusantium alias repellendus minimaharum, similique ea mollitia sit omnis deleniti totam perferendis!',
-  view: [' 30.000', ' 20.000', ' 10.000'],
-};
+const dummyBlog = [
+  'resources/images/dummyimage2.jpg',
+  '1. Super ultra hot posts',
+  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet,repudiandae excepturi ducimus animi omnis rerum? Accusantium alias repellendus minimaharum, similique ea mollitia sit omnis deleniti totam perferendis!',
+  '10.000',
+];
+
+var image = document.getElementsByClassName('post-image');
+for (var i = 0; i < image.length; i++) {
+  image[i].setAttribute('src', dummyBlog[0]);
+}
 
 var title = document.getElementsByClassName('post-title');
 for (var i = 0; i < title.length; i++) {
-  var text = document.createTextNode(dummyBlog.title[i]);
+  var text = document.createTextNode(dummyBlog[1]);
   title[i].appendChild(text);
 }
 
 var content = document.getElementsByClassName('post-content');
 for (var i = 0; i < content.length; i++) {
-  var text = document.createTextNode(dummyBlog.content);
+  var text = document.createTextNode(dummyBlog[2]);
   content[i].appendChild(text);
 }
 
 var view = document.getElementsByClassName('post-view');
 for (var i = 0; i < view.length; i++) {
-  var text = document.createTextNode(dummyBlog.view[i]);
+  var text = document.createTextNode(dummyBlog[3]);
   view[i].appendChild(text);
-}
-
-var image = document.getElementsByClassName('post-image');
-for (var i = 0; i < image.length; i++) {
-  image[i].setAttribute('src', dummyBlog.image[i]);
 }
