22a8a62ab9e9bfc24da558159200e04a60945ec0
Sin <mrsin0916@gmail.com>
2019-11-22 00:19:12 +0700

    Improve render Improve responsive
    Add js  Change css Less html

--- resources/js/blog.js
+++ resources/js/blog.js
@@ -1,29 +1,66 @@
 const dummyBlog = [
-  'resources/images/dummyimage2.jpg',
-  '1. Super ultra hot posts',
-  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet,repudiandae excepturi ducimus animi omnis rerum? Accusantium alias repellendus minimaharum, similique ea mollitia sit omnis deleniti totam perferendis!',
-  '10.000',
+  {
+    image: 'resources/images/dummyimage2.jpg',
+    title: '1. Super ultra hot posts',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet,repudiandae excepturi ducimus animi omnis rerum? Accusantium alias repellendus minimaharum, similique ea mollitia sit omnis deleniti totam perferendis!',
+    view: ' 30.000',
+  },
+  {
+    image: 'resources/images/dummyimage3.jpg',
+    title: '2. Super ultra hot posts',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet,repudiandae excepturi ducimus animi omnis rerum? Accusantium alias repellendus minimaharum, similique ea mollitia sit omnis deleniti totam perferendis!',
+    view: ' 20.000',
+  },
+  {
+    image: 'resources/images/dummyimage4.jpg',
+    title: '3. Super ultra hot posts',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime voluptatibus eveniet,repudiandae excepturi ducimus animi omnis rerum? Accusantium alias repellendus minimaharum, similique ea mollitia sit omnis deleniti totam perferendis!',
+    view: ' 10.000',
+  },
 ];
 
-var image = document.getElementsByClassName('post-image');
-for (var i = 0; i < image.length; i++) {
-  image[i].setAttribute('src', dummyBlog[0]);
-}
+for (var i = 0; i < 3; i++) {
+  var icon = document.createElement('I');
+  var postview = document.createElement('DIV');
+  var postcontent = document.createElement('DIV');
+  var posttitle = document.createElement('DIV');
+  var content = document.createElement('DIV');
+  var postimage = document.createElement('IMG');
+  var card = document.createElement('DIV');
+  var cardcontainer = document.createElement('DIV');
+  var listcard = document.getElementsByClassName('card-list');
 
-var title = document.getElementsByClassName('post-title');
-for (var i = 0; i < title.length; i++) {
-  var text = document.createTextNode(dummyBlog[1]);
-  title[i].appendChild(text);
-}
+  postview.classList.add('post-view');
+  icon.className = 'fas fa-eye';
+  postview.appendChild(icon);
+  var pview = document.createTextNode(dummyBlog[i].view);
+  postview.appendChild(pview);
 
-var content = document.getElementsByClassName('post-content');
-for (var i = 0; i < content.length; i++) {
-  var text = document.createTextNode(dummyBlog[2]);
-  content[i].appendChild(text);
-}
+  postcontent.classList.add('post-content');
+  var pcontent = document.createTextNode(dummyBlog[i].content);
+  postcontent.appendChild(pcontent);
 
-var view = document.getElementsByClassName('post-view');
-for (var i = 0; i < view.length; i++) {
-  var text = document.createTextNode(dummyBlog[3]);
-  view[i].appendChild(text);
+  posttitle.classList.add('post-title');
+  var ptitle = document.createTextNode(dummyBlog[i].title);
+  posttitle.appendChild(ptitle);
+
+  content.classList.add('content');
+  content.appendChild(posttitle);
+  content.appendChild(postcontent);
+  content.appendChild(postview);
+
+  postimage.classList.add('post-image');
+  postimage.setAttribute('src', dummyBlog[i].image);
+
+  card.classList.add('card');
+  card.appendChild(postimage);
+  card.appendChild(content);
+
+  cardcontainer.classList.add('card-container');
+  cardcontainer.appendChild(card);
+
+  listcard[0].appendChild(cardcontainer);
 }
