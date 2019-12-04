18275ad342c196d4560288762ffca859cb0c68c6
Ngoc Nguyen Minh <ngoc.nguyenminh@hcmut.edu.vn>
2019-12-04 21:02:15 +0700

    rebuild cover block

---
+++ resources/js/cover.js
@@ -0,0 +1,13 @@
+const str = 'ÔNG DEV VUI TÍNH';
+
+var titleContainer = document.getElementsByClassName('cover-title');
+var title = document.createElement('H1');
+titleContainer[0].appendChild(title);
+
+function add(i) {
+  if(i>=str.length) return;
+	title.innerHTML += str[i];
+  setTimeout(add,200,i+1);
+}
+add(0);
+
