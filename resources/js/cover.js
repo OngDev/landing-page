1d4d91af9e80efc71a777e63fd891f85fdbd1357
Ngoc Nguyen Minh <ngoc.nguyenminh@hcmut.edu.vn>
2019-12-05 12:36:49 +0700

    change into ES6 in cover.js

--- resources/js/cover.js
+++ resources/js/cover.js
@@ -4,10 +4,10 @@ var titleContainer = document.getElementsByClassName('cover-title');
 var title = document.createElement('H1');
 titleContainer[0].appendChild(title);
 
-function add(i) {
+add = (i) => {
   if(i >= str.length) return;
 	title.innerHTML += str[i];
-  setTimeout(add, 200, i+1);
+  setTimeout(add, 250, i+1);
 }
 
 add(0);
