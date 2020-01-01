07ea6847152ca7f6714bda54542a3776ca425e04
Sin <mrsin0916@gmail.com>
2020-01-01 14:34:36 +0700

    Build layout login and register modal

--- resources/js/header.js
+++ resources/js/header.js
@@ -1,9 +1,13 @@
+var body = document.getElementsByTagName('BODY')[0];
 var popup = document.getElementById('popup');
+var form = document.getElementById('form');
 
 showPopup = () => {
   popup.style.display = 'block';
+  form.style.display = 'flex';
 };
 
 hidePopup = () => {
   popup.style.display = 'none';
+  form.style.display = 'none';
 };
