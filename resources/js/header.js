12f18e17a12d99beceaba21f77092bc3d7dc6602
Sin <mrsin0916@gmail.com>
2020-01-07 21:02:53 +0700

    Improve login and register modal
    add more thing

--- resources/js/header.js
+++ resources/js/header.js
@@ -1,13 +1,24 @@
 var body = document.getElementsByTagName('BODY')[0];
 var popup = document.getElementById('popup');
 var form = document.getElementById('form');
+var login = document.getElementById('login');
+var regis = document.getElementById('register');
+var typeForm = document.getElementsByClassName('type-form');
 
-showPopup = () => {
-  popup.style.display = 'block';
-  form.style.display = 'flex';
+togglePopup = () => {
+  popup.classList.toggle('d-flex');
+  form.classList.toggle('d-flex');
+  login.style.display = 'none';
+  register.style.display = 'flex';
 };
 
-hidePopup = () => {
-  popup.style.display = 'none';
-  form.style.display = 'none';
+changeForm = () => {
+  login.style.display == 'none' ? (login.style.display = 'flex') : (login.style.display = 'none');
+  regis.style.display == 'flex' ? (regis.style.display = 'none') : (regis.style.display = 'flex');
+  typeForm[0].style.display == 'none'
+    ? (typeForm[0].style.display = 'flex')
+    : (typeForm[0].style.display = 'none');
+  typeForm[1].style.display == 'flex'
+    ? (typeForm[1].style.display = 'none')
+    : (typeForm[1].style.display = 'flex');
 };
