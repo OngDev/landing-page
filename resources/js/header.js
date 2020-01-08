a938885c84ee86ac471a1af56fc1cdab361d759e
Sin <mrsin0916@gmail.com>
2020-01-08 23:16:05 +0700

    Improve login and register modal
    add some thing

--- resources/js/header.js
+++ resources/js/header.js
@@ -2,23 +2,38 @@ var body = document.getElementsByTagName('BODY')[0];
 var popup = document.getElementById('popup');
 var form = document.getElementById('form');
 var login = document.getElementById('login');
-var regis = document.getElementById('register');
+var register = document.getElementById('register');
+var have = document.getElementById('have');
+var need = document.getElementById('need');
+var forgot = document.getElementById('forgot');
 var typeForm = document.getElementsByClassName('type-form');
 
 togglePopup = () => {
   popup.classList.toggle('d-flex');
   form.classList.toggle('d-flex');
-  login.style.display = 'none';
-  register.style.display = 'flex';
+  login.style.display = 'flex';
+  register.style.display = 'none';
+  have.style.display = 'none';
+  need.style.display = 'flex';
+  forgot.style.display = 'flex';
 };
 
 changeForm = () => {
-  login.style.display == 'none' ? (login.style.display = 'flex') : (login.style.display = 'none');
-  regis.style.display == 'flex' ? (regis.style.display = 'none') : (regis.style.display = 'flex');
-  typeForm[0].style.display == 'none'
-    ? (typeForm[0].style.display = 'flex')
-    : (typeForm[0].style.display = 'none');
-  typeForm[1].style.display == 'flex'
-    ? (typeForm[1].style.display = 'none')
-    : (typeForm[1].style.display = 'flex');
+  if (login.style.display == 'flex' || register.style.display == 'none') {
+    typeForm[0].style.display = 'none';
+    typeForm[1].style.display = 'flex';
+    login.style.display = 'none';
+    register.style.display = 'flex';
+    have.style.display = 'flex';
+    need.style.display = 'none';
+    forgot.style.display = 'none';
+  } else if (login.style.display == 'none' || register.style.display == 'flex') {
+    typeForm[0].style.display = 'flex';
+    typeForm[1].style.display = 'none';
+    login.style.display = 'flex';
+    register.style.display = 'none';
+    have.style.display = 'none';
+    need.style.display = 'flex';
+    forgot.style.display = 'flex';
+  }
 };
